f7f470518aa969db9e62efa5baba30a501e3aa01
Abishek-Chakravarthy <133603690+Abishek-Chakravarthy@users.noreply.github.com>
2023-08-24 00:35:50 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+st.write("""
+#  App
+
+This app predicts the largest of Three Numbers
+""")
+#Get Input
+
+st.header('User Input Parameters')
+
+def user_input_features():
+    a = st.number_input("First Number",min_value=2.0,max_value=2000000.0)
+    b = st.number_input("Second Number",min_value=2.0,max_value=2000000.0)
+    c = st.number_input("Third Number",min_value=2.0,max_value=2000000.0)
+    data = {'First Number':a,'Second Number':b,'Third Number':c}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df.to_dict())
+
+st.subheader('Prediction')
+if a>=b and a>=c:
+    st.write('The largest number is',a)
+elif b>=a and b>=c:
+    st.write('The largest number is',b)
+else:
+    st.write('The largest number is',c)
+#st.write(prediction)
